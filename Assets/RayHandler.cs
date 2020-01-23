abf80cb9ba2bb1e4b6cb000349c93cbab71ebb4c
ElijahZAwesome <>
2020-01-22 23:48:07 -0600

    Menu working, try to get raycast working

---
+++ Assets/RayHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayHandler : MonoBehaviour
+{
+
+    [Range(0, 10)]
+    public float TargetOffset = 1f;
+    public Transform Target;
+    public Transform OriginPoint;
+    public LineRenderer Renderer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Assert(Target != null);
+        Debug.Assert(OriginPoint != null);
+        Debug.Assert(Renderer != null);
+
+        Renderer.SetPositions(new Vector3[] { Target.position + (Target.forward * TargetOffset), OriginPoint.position });
+    }
+}
