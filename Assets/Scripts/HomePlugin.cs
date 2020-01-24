4419323f99ce932464f91c7cf3d77ff9b2f0d7a1
ElijahZAwesome <>
2020-01-24 16:06:04 -0600

    Almost working java plugin

---
+++ Assets/Scripts/HomePlugin.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SettingsPlugin
+{
+    public SettingsPlugin()
+    {
+
+    }
+
+    public static string GetHomePackage()
+    {
+        AndroidJNIHelper.debug = true;
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            using (AndroidJavaClass jc = new AndroidJavaClass("com.elijahzawesome.homeplugin.AndroidPlugin"))
+            {
+                return jc.Call<string>("GetHomeUri");
+            }
+        } 
+        else
+        {
+            return "currently not running on android.";
+        }
+
+        return "error";
+    }
+}
---
+++ HomePlugin/src/main/java/com/elijahzawesome/homeplugin/AndroidPlugin.java
@@ -0,0 +1,7 @@
+package com.elijahzawesome.homeplugin;
+
+public class AndroidPlugin {
+    public String GetHomeUri() {
+        return "test";
+    }
+}
